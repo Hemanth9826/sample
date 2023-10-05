69cfb020dff40737c45c57f632ff280f47ebae99
Hemanth9826 <106152898+Hemanth9826@users.noreply.github.com>
2023-10-05 13:15:41 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Socket.IO server running on port 3000');
+});
