d1776f367b5c0577f3d61b8a0e5f9470284b578d
Hemanth9826 <106152898+Hemanth9826@users.noreply.github.com>
2023-10-09 10:54:03 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,43 +1,63 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const { Connection, PlatformEventSubscription } = require('jsforce');
+const { PubSub } = require('@google-cloud/pubsub');
+const jsforce = require('jsforce');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Salesforce connection
-const conn = new Connection({
-  loginUrl: 'https://login.salesforce.com',
-  accessToken: 'YOUR_ACCESS_TOKEN', // Replace with your Salesforce access token
+// Google Cloud Pub/Sub configuration
+const pubsub = new PubSub();
+const subscriptionName = 'your-subscription-name';
+const subscription = pubsub.subscription(subscriptionName);
+
+// Salesforce configuration
+const sfUsername = 'your-salesforce-username';
+const sfPassword = 'your-salesforce-password';
+const sfSecurityToken = 'your-salesforce-security-token';
+const conn = new jsforce.Connection();
+
+// Express.js routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Connect to Salesforce and subscribe to the Platform Event
-conn.login()
-  .then(() => {
-    const subscription = conn.streaming.topic('AccountCreatedEvent').subscribe(
-      (message) => {
-        const accountName = message.sobject.AccountName__c;
-        io.emit('newAccount', { accountName });
-      }
-    );
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-    // Handle disconnects and errors
-    subscription.on('error', (error) => {
-      console.error('Error:', error);
-    });
-
-    subscription.on('disconnect', () => {
-      console.log('Disconnected from Salesforce');
-    });
-  })
-  .catch((error) => {
-    console.error('Salesforce login error:', error);
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 
-// Serve static files or set up routes as needed
+  subscription.on('message', (message) => {
+    // Handle the incoming Pub/Sub message here.
+    const messageData = message.data.toString();
+    io.emit('message', messageData);
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    // Update Salesforce (example: create a lead)
+    conn.login(sfUsername, sfPassword + sfSecurityToken, (err, userInfo) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Logged in as ${userInfo.id}`);
+        const lead = { FirstName: 'John', LastName: 'Doe', Company: 'Example Inc' };
+        conn.sobject('Lead').create(lead, (err, ret) => {
+          if (err || !ret.success) {
+            console.error(err || ret);
+          } else {
+            console.log(`Created Salesforce Lead ID: ${ret.id}`);
+          }
+        });
+      }
+    });
+
+    message.ack();
+  });
+});
+
+server.listen(6060, () => {
+  console.log('Server listening on port 6060');
 });
